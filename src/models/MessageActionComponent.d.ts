8b6feec60bf235aaee795d56b4a484a98847792e
OctSquid <pqvs6k96@s.okayama-u.ac.jp>
2023-02-16 07:31:34 +0900

    🏷️ Message Component の Execute 型に修正

--- src/models/MessageActionComponent.d.ts
+++ src/models/MessageActionComponent.d.ts
@@ -1,11 +1,11 @@
 import { MessageActionRow, MessageComponentInteraction } from 'discord.js';
 
-type MessageActionComponentExecuteOptions = {
-  interaction: MessageComponentInteraction;
-};
+export type MessageActionComponentExecute = (
+  interaction: MessageComponentInteraction,
+) => Promise<void>;
 
 export interface MessageActionComponent {
   id: string;
   view: (options: any) => Promise<MessageActionRow>;
-  execute: (options: MessageActionComponentExecuteOptions) => Promise<void>;
+  execute: MessageActionComponentExecute;
 }
