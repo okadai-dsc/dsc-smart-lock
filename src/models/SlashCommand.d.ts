e48333408426ad07a07cebf39b9183d7ab08bdc7
OctSquid <pqvs6k96@s.okayama-u.ac.jp>
2023-02-16 20:13:49 +0900

    🏷️ コマンドの実行許可設定のプロパティを追加

--- src/models/SlashCommand.d.ts
+++ src/models/SlashCommand.d.ts
@@ -3,6 +3,10 @@ import { ApplicationCommandData, CommandInteraction } from 'discord.js';
 export type CommandExecute = (interaction: CommandInteraction) => Promise<void>;
 
 export interface SlashCommand {
+  /** DMでの使用を許可するか */
+  allowDM?: boolean;
+  /** 使用が許可されるロールID */
+  allowedRoleId?: string;
   data: ApplicationCommandData;
   execute: CommandExecute;
 }
